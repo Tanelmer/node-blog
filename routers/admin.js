1b3d3466e70297c6cde49ef6edf64fd06721f60a
lixinyi <244lixinyi@163.com>
2018-04-14 17:55:03 +0800

    add manage user

--- routers/admin.js
+++ routers/admin.js
@@ -3,6 +3,8 @@
  */
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
+
 
 router.use(function(req,res,next) {
 	if(!req.userInfo.isAdmin){
@@ -11,10 +13,23 @@ router.use(function(req,res,next) {
 	next();
 });
 
+//admin首页
 router.get('/',function (req,res,next) {
 	return res.render('admin/index',{
 		user:req.userInfo
 	});
+});
+
+//用户管理
+router.get('/user',function (req,res,next) {
+	return res.render('admin/manage-user',{
+		user:req.userInfo
+	});
+});
+
+//获取用户表中数据
+User.find().then(function (users) {
+	
 });
 
 module.exports = router;
